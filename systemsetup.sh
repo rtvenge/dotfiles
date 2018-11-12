d63c973a43da241b83bfcff4a1c38f87efacc165
Ryan Tvenge <ryan@hoverboardstudios.com>
2018-11-12 11:33:34 -0800

    Cleaning up dotfiles

--- systemsetup.sh
+++ systemsetup.sh
@@ -9,10 +9,6 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 # Install needed homebrew packages
 ~/dotfiles/.brew
 
-#rvm for ruby docs: http://rvm.io/rvm/install
-# ??? gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
-curl -sSL https://get.rvm.io | bash -s stable
-
 #wp-cli
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
@@ -24,9 +20,6 @@ curl https://raw.githubusercontent.com/creationix/nvm/v0.18.0/install.sh | bash
 # Install needed node modules
 ~/dotfiles/.nodemodules
 
-# Install needed Ruby Gems
-~/dotfiles/.gems
-
 #symlink from dotfiles directory
 ~/dotfiles/symlink_files.sh
 
@@ -35,5 +28,3 @@ touch ~/.hushlogin
 
 # install ohmyzsh
 curl -L http://install.ohmyz.sh | sh
-# subl command
-ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
