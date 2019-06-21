e098caa337d8683dd1429f02e5c7cafe93bfe776
Ryan Tvenge <ryan@hoverboardstudios.com>
2019-06-21 12:52:08 -0700

    Adding google api

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,6 +12,8 @@ export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
 export PATH="$PATH:/Applications/DevDesktop/mysql/bin"
 export PATH="$PATH:/Users/ryantvenge/vendor/bin:$PATH"
 export EDITOR="/usr/local/bin/vim"
+export WHOIS_XML_API_KEY="at_Y0y3t70W59yrLgSuANeQUKXMg4jnV"
+export GOOGLE_API_KEY="AIzaSyC4OrnzjcoHySGi5XFjtWOw3qkmSJ1BvBk"
 export NVM_DIR="$HOME/.nvm"
 export PATH="$HOME/.composer/vendor/bin:$PATH"
 # [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
