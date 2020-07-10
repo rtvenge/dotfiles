1e1e799b88ebba51290cc3f0a885af921203fcd9
Ryan Tvenge <ryan@hoverboardstudios.com>
2020-07-10 16:58:55 -0700

    Adding light theme option.

--- .hyper.js
+++ .hyper.js
@@ -1,19 +1,20 @@
 module.exports = {
   config: {
-    shell: '/usr/local/bin/fish',
+    shell: "/usr/local/bin/fish",
 
     // Appearance
-    fontFamily: '"Perplexed", "Source Code Pro for Powerline", "DejaVu Sans Mono", "Lucida Console", monospace',
-    cursorColor: '#f4d300',
-    backgroundColor: '#173448',
-    borderColor: '#ffc600',
-    wickedBorderColor: '#173448',
+    fontFamily:
+      '"Perplexed", "Source Code Pro for Powerline", "DejaVu Sans Mono", "Lucida Console", monospace',
+    cursorColor: "#f4d300",
+    backgroundColor: "#173448",
+    borderColor: "#ffc600",
+    wickedBorderColor: "#173448",
     fontSize: 14,
-    cursorShape: 'BEAM',
+    cursorShape: "BEAM",
 
     // Style
-    css: '',
-    padding: '4px',
+    css: "",
+    padding: "4px",
     windowSize: [1024, 768],
 
     // Behavior
@@ -22,11 +23,12 @@ module.exports = {
   },
 
   plugins: [
-    'hyperterm-1password',
-    'hyperterm-dibdabs',
-    'hyperterm-bold-tab',
-    'hyperterm-title',
-    'hyper-search',
-    'hyperterm-cobalt2-theme',
+    "hyperterm-1password",
+    "hyperterm-dibdabs",
+    "hyperterm-bold-tab",
+    "hyperterm-title",
+    "hyper-search",
+    // 'hyperterm-cobalt2-theme',
+    "hyper-hypest",
   ],
 };
