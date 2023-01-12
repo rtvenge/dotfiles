93dda0a328562ec7729c222f306b290863baa2b8
Ryan Tvenge <ryan@hoverboardstudios.com>
2023-01-12 08:19:27 -0600

    Upgrading to espanso 2.x.x.

--- espanso/scripts/admin.py
+++ espanso/scripts/admin.py
@@ -2,7 +2,8 @@
 
 import re
 import os
-import requests
+# espanso 2.x.x requests couldn't be loaded
+# import requests
 
 replacement = re.search('https?:\/\/[^\/]*', os.environ["ESPANSO_CLIPBOARD"])
 
@@ -11,9 +12,11 @@ if 'lndo.site' in replacement.group(0):
 elif 'hbserver.dev' in replacement.group(0):
   print(replacement.group(0) + '/hb_admin')
 else:
-  resp = requests.get(replacement.group(0) + '/admin')
+  # espanso 2.x.x requests couldn't be loaded
+  # resp = requests.get(replacement.group(0) + '/admin')
 
-  if resp.status_code == 404:
-    print(replacement.group(0) + '/hb_admin')
-  else:
-    print(replacement.group(0) + '/admin')
+  # if resp.status_code == 404:
+  #   print(replacement.group(0) + '/hb_admin')
+  # else:
+  #   print(replacement.group(0) + '/admin')
+  print(replacement.group(0) + '/hb_admin')
