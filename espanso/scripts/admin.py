a0ce39d8178325dae6ee4c4f7b850dc086be3440
Ryan Tvenge <ryan@hoverboardstudios.com>
2023-02-02 10:20:54 -0600

    Readding request; Works now for some reason.

--- espanso/scripts/admin.py
+++ espanso/scripts/admin.py
@@ -2,8 +2,7 @@
 
 import re
 import os
-# espanso 2.x.x requests couldn't be loaded
-# import requests
+import requests
 
 replacement = re.search('https?:\/\/[^\/]*', os.environ["ESPANSO_CLIPBOARD"])
 
@@ -12,11 +11,9 @@ if 'lndo.site' in replacement.group(0):
 elif 'hbserver.dev' in replacement.group(0):
   print(replacement.group(0) + '/hb_admin')
 else:
-  # espanso 2.x.x requests couldn't be loaded
-  # resp = requests.get(replacement.group(0) + '/admin')
+  resp = requests.get(replacement.group(0) + '/admin')
 
-  # if resp.status_code == 404:
-  #   print(replacement.group(0) + '/hb_admin')
-  # else:
-  #   print(replacement.group(0) + '/admin')
-  print(replacement.group(0) + '/hb_admin')
+  if resp.status_code == 404:
+    print(replacement.group(0) + '/hb_admin')
+  else:
+    print(replacement.group(0) + '/admin')
