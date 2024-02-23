691ee77d0fea7a2d2dbb226956659c14b795e040
Ryan Tvenge <ryan@hoverboardstudios.com>
2024-02-23 15:13:38 -0600

    Updating setup.

--- bin/systemsetup.sh
+++ bin/systemsetup.sh
@@ -6,27 +6,15 @@ xcode-select --install
 #homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Install needed homebrew packages
-~/dotfiles/.brew
+xargs brew install < ~/dotfiles/brew/leaves.txt
 
 git clone https://github.com/bobthecow/git-flow-completion ~/.oh-my-zsh/custom/plugins/git-flow-completion
-
-#wp-cli
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-sudo mv wp-cli.phar /usr/local/bin/wp
 
 #nvm
 curl https://raw.githubusercontent.com/creationix/nvm/v0.18.0/install.sh | bash
 
-# Install needed node modules
-~/dotfiles/.nodemodules
-
 #symlink from dotfiles directory
-~/dotfiles/symlink_files.sh
-
-# Disable "last logged in" prompt
-touch ~/.hushlogin
+stow --dir=~/dotfiles --target=~/
 
 # Install OMZ
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
