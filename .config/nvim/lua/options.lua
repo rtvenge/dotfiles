7f628fb4ca2f2064c6dca8340ca9f0621c50a3ac
Ryan Tvenge <ryan@hoverboardstudios.com>
2024-02-28 17:20:25 -0600

    Organizing lua files in includes.

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,49 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- TODO: Remove?
+vim.g.copilot_no_tab_map = true
+vim.opt.termguicolors = true
+
+-- Set highlight on search
+vim.o.hlsearch = false
+
+-- Make line numbers default
+vim.wo.number = true
+
+-- Enable mouse mode
+vim.o.mouse = 'a'
+
+vim.opt.list = true
+vim.opt.listchars = { tab = "▸\\ ", trail = "·", space = "·" }
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.o.clipboard = 'unnamedplus'
+
+-- Enable break indent
+vim.o.breakindent = true
+
+vim.g.nvim_tree_auto_open = 1
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case-insensitive searching UNLESS \C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = 'yes'
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+vim.o.relativenumber = true
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
+
+-- NOTE: You should make sure your terminal supports this
+vim.o.termguicolors = true
