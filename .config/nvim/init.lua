1c62482bcd4b54385e7cb230bc8ebd40bef6e65b
Ryan Tvenge <ryan@hoverboardstudios.com>
2024-02-28 17:24:46 -0600

    Moving last chunk to include.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,17 +9,5 @@ require('keymaps')
 require('options')
 require('lsp')
 
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
-})
-
--- Setup neovim lua configuration
-require('neodev').setup()
-
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -47,3 +47,15 @@ vim.o.completeopt = 'menuone,noselect'
 
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
+
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
+-- Setup neovim lua configuration
+require('neodev').setup()
