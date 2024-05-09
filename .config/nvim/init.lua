22c18f199919dbc58b5940d7d4018ee1b306bfbb
Ryan Tvenge <ryan@hoverboardstudios.com>
2024-05-09 10:19:24 -0500

    WIP: Adding snippets; not loading.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,6 +8,6 @@ require('plugins.cmp')
 require('keymaps')
 require('options')
 require('lsp')
-
+require('luasnip')
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- .config/nvim/lua/plugins/lazy.lua
+++ .config/nvim/lua/plugins/lazy.lua
@@ -23,14 +23,28 @@ require('lazy').setup({
   'mattn/emmet-vim',
   'vim-syntastic/syntastic',
   'dense-analysis/ale',
+  'rafamadriz/friendly-snippets',
+
   {
     'Exafunction/codeium.vim',
     config = function ()
       vim.keymap.set('i', '<C-j>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
-      vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
-      vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
+      --vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
+      --vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
       vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
     end
+  },
+  {
+	"L3MON4D3/LuaSnip",
+    -- follow latest release.
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    config = function()
+      -- These aren't loading
+      require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/lua/snippets" } })
+    end,
   },
   {
     'neovim/nvim-lspconfig',
