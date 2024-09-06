6df276fd3e5152715188bfe70d2892d01eef6bb5
Ryan Tvenge <ryan@hoverboardstudios.com>
2024-09-06 10:46:57 -0500

    Moving to lazyvim.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,2 @@
-require('plugins.lazy')
-require('plugins.telescope')
-require('plugins.treesitter')
-require('plugins.nvim-tree')
-require('plugins.which-key')
-require('plugins.mason')
-require('plugins.cmp')
-require('keymaps')
-require('options')
-require('lsp')
-require('luasnip')
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
