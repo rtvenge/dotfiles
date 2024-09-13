329f697cc2ed939b5dbe98685d224bd5309cb353
Ryan Tvenge <ryan@hoverboardstudios.com>
2024-09-13 10:00:12 -0500

    Enabling config file.

--- .config/nvim/lua/plugins/config.lua
+++ .config/nvim/lua/plugins/config.lua
@@ -1,7 +1,3 @@
--- since this is just an example spec, don't actually load anything here and return an empty spec
--- stylua: ignore
-if true then return {} end
-
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
 -- In your plugin files, you can:
@@ -26,6 +22,8 @@ return {
 
   -- disable trouble
   { "folke/trouble.nvim", enabled = false },
+
+  { "dense-analysis/ale" },
 
   -- override nvim-cmp and add cmp-emoji
   {
